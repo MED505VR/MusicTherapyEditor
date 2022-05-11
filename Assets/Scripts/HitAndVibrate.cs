e6ecfb796061427ad459906a81876fd83660aa05
Jacob Noer <jnoer19@student.aau.dk>
2022-05-11 21:17:50 +0200

    Fixed pivot point

--- Assets/Scripts/HitAndVibrate.cs
+++ Assets/Scripts/HitAndVibrate.cs
@@ -3,6 +3,7 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.XR;
+using UnityEngine.XR.Interaction.Toolkit;
 
 
 
@@ -12,13 +13,23 @@ public class HitAndVibrate : MonoBehaviour
     private AudioSource SoundAudioSource { get; set; }
     [SerializeField]
     private AudioClip _soundClip;
+
+    //[SerializeField] private float amplitude;
+    //[SerializeField] private float duration;
+
+    //private XRController _xr;
+    
     // Start is called before the first frame update
     void Start()
     {
         SoundAudioSource = GetComponent<AudioSource>();
         SoundAudioSource.clip = _soundClip;
+        //_xr = GetComponent<XRController>();
     }
-    
+    //void ActivateHaptic()
+    //{
+        //_xr.SendHapticImpulse(0.7f, 2f);
+    //}
 
     private void OnTriggerEnter(Collider other)
     {
@@ -26,6 +37,7 @@ public class HitAndVibrate : MonoBehaviour
         {
             SoundAudioSource.Play();
             print("Hit");
+            //ActivateHaptic();
         }
     }
 }
